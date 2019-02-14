5bec83040ea5d22231a32dffec898828f406c66e
mcbeem <mmcbee@gmail.com>
2019-02-14 08:04:46 -0500

    readme and Rbuildignore changes

--- R/plot_regsim_method.R
+++ R/plot_regsim_method.R
@@ -8,6 +8,8 @@
 #' @param fixedStyle numeric or vector of an lty and a color for the arrows. defaults to 1 for black, solid lines. Applies only to \code{type="model"}.
 #' @param nCharNodes numeric; controls how many characters of each variable's name to display. defaults to
 #'    zero so that the full name is shown. Applies only to \code{type="model"}.
+#' @param whatLabels character, controls what labels are shown in the model diagram. Must be one of \code{"name"},
+#'   \code{"est"}, \code{"stand"}, \code{"eq"}, or \code{"no"}. Defaults to \code{"est"}. Applies only to \code{type="model"}.
 #' @param residuals logical, controls whether the residuals should be displayed in the model plot.
 #'    Applies only to \code{type="model"}.
 #' @param ... additional arguments to be passed to \code{semPlot::semPaths} (for \code{type="model"}),
@@ -17,13 +19,13 @@
 #' @export
 
 plot.regsim <- function(x, type="perf", sizeMan=10, edge.label.cex=1.5,
-                        fixedStyle=1, nCharNodes=0, residuals=FALSE, ...) {
+                        fixedStyle=1, nCharNodes=0, whatLabels="est", residuals=FALSE, ...) {
 
   if (max(!type %in% c("model", "perf", "data", "compareSE"))) stop("Argument type must only contain \"model\", \"perf\", \"data\", or \"compareSE\"")
 
   if (max(type %in% "model")) {
-    semPlot::semPaths(semPlot::semPlotModel(x$true.model), whatLabels="est",
-           exoCov=FALSE, sizeMan=sizeMan, edge.label.cex=edge.label.cex, residuals=residuals,
+    semPlot::semPaths(semPlot::semPlotModel(x$true.model), whatLabels=whatLabels,
+           exoCov=FALSE, sizeMan=sizeMan, sizeLat=sizeMan, edge.label.cex=edge.label.cex, residuals=residuals,
              fixedStyle=fixedStyle, nCharNodes=nCharNodes, ...)}
 
   if (max(type %in% "perf")) {
